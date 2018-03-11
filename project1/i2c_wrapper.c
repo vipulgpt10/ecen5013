e05b914a1bed24f6dcaa0ad1b559234ffde752d4
Vipul Gupta <vipul.gupta@colorado.edu>
2018-03-10 19:14:07 -0700

    Project 1: i2c warapper bare minimum

---
+++ project1/i2c_wrapper.c
@@ -0,0 +1,12 @@
+/**
+ * @file i2c_wrapper.c
+ * @brief The implementation file for i2c wrapper functions
+ *
+ * This implementation file provides the function definitions for
+ * various i2c wrapper operations.
+ *
+ * @author Vipul Gupta
+ * @date 09 March 2018
+ *
+ */
+
---
+++ project1/i2c_wrapper.h
@@ -0,0 +1,31 @@
+/**
+ * @file i2c_wrapper.h
+ * @brief An abtraction for i2c wrapper function
+ *
+ * This header file provides the prototypes of the function for
+ * various i2c operations using mraa/i2c library.
+ *
+ * @author Vipul Gupta
+ * @date 09 March 2018
+ *
+ */
+
+#ifndef __I2C_WRAPPER_H__
+#define __I2C_WRAPPER_H__
+
+/**
+ * @brief Moves the data from source to destination
+ *
+ * Given the source and destination pointers and the length of the data,
+ * it will move the data from source to destination and return the 
+ * pointer to the destination.
+ *
+ * @param src Pointer to the source
+ * @param dst Pointer to the destination
+ * @param length Size of the data in bytes
+ *
+ * @return Pointer to the destination
+ */
+
+
+#endif	/* __I2C_WRAPPER_H__ */
