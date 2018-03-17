c27de458f8b422b5d51d27774089e80d57dc645f
Vipul Gupta <vipul.gupta@colorado.edu>
2018-03-17 00:31:59 -0600

    LED switching functions added

---
+++ project1/include/bbg/led_task.h
@@ -0,0 +1,40 @@
+/**
+ * @file led_task.h
+ * @brief An abtraction for USR LED switching functions
+ *
+ * This header file provides the prototypes of the function for
+ * switching USR LED0 on Beaglebone Green.
+ *
+ * @author Vipul Gupta
+ * @date 17 March 2018
+ *
+ */
+
+#ifndef __LIGHT_TASK_H__
+#define __LIGHT_TASK_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Blue USR LED0 but green name! */
+#define LED0_PATH "/sys/class/leds/beaglebone:green:usr0/brightness"
+
+
+/**
+ * @brief Turns on USR LED0
+ *
+ * It turns OFF the USR LED0 on Beaglebone Green.
+ *
+ */
+void LED_ON(void);
+
+/**
+ * @brief Turns off USR LED0
+ *
+ * It turns OFF the USR LED0 on Beaglebone Green.
+ *
+ */
+void LED_OFF(void);
+
+#endif	/* __LIGHT_TASK_H__ */
---
+++ project1/src/led_task.c
@@ -0,0 +1,43 @@
+/**
+ * @file led_task.c
+ * @brief The implementation file for USR LED switching functions
+ *
+ * This implementation file provides the function definitions for
+ * switching USR LED0 on Beaglebone Green.
+ *
+ * @author Vipul Gupta
+ * @date 17 March 2018
+ *
+ */
+
+#include "led_task.h"
+
+void LED_ON(void)
+{
+	FILE * fptr = fopen(LED0_PATH, "w+");
+	
+	if(fptr == NULL)
+		return;
+	else
+	{
+		fputc('1', fptr);
+		fclose(fptr);
+		return;	
+	}
+}
+
+void LED_OFF(void)
+{
+	FILE * fptr = fopen(LED0_PATH, "w+");
+	
+	if(fptr == NULL)
+		return;
+	else
+	{
+		fputc('0', fptr);
+		fclose(fptr);
+		return;	
+	}
+}
+
+
